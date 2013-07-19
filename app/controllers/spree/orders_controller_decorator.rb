d7dc50a1f988f17dd3678ccabcff791dc4218565
Harris Novick <harris@harrisnovick.com>
2013-07-18 23:23:24 -0400

    ed-specific stuff

--- app/controllers/spree/orders_controller_decorator.rb
+++ app/controllers/spree/orders_controller_decorator.rb
@@ -18,8 +18,8 @@ module Spree
         @order.add_variant(Variant.find(variant_id), quantity, ad_hoc_option_value_ids, product_customizations) if quantity > 0
       end if params[:products]
 
-      params[:variants].each do |variant_id, quantity|
-        quantity = quantity.to_i
+      params[:variants].each do |_,variant_id|
+        quantity = params[:quantity].to_i
         @order.add_variant(Variant.find(variant_id), quantity, ad_hoc_option_value_ids, product_customizations) if quantity > 0
       end if params[:variants]
 
