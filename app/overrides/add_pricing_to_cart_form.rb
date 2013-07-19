96abdde719aab183fd39c228a317cee6559a3a3b
Harris Novick <harris@harrisnovick.com>
2013-07-18 23:38:57 -0400

    ed-specific cart form stuff

--- app/overrides/add_pricing_to_cart_form.rb
+++ app/overrides/add_pricing_to_cart_form.rb
@@ -1,4 +1,4 @@
 Deface::Override.new(:virtual_path => "spree/products/_cart_form",
                          :name => "converted_product_price_331970321",
-                         :insert_after => "[data-hook='product_price'], #product_price[data-hook]",
+                         :insert_after => "[data-hook='product_options'], #product_variants[data-hook]",
                          :partial => "spree/products/pricing")
